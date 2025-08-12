0b09ccaafbe958e24e8c02d959a223c7690274f0
gabrielmatteojones <gabriel.matteo.jones@18f.it>
2025-08-12 11:02:41 +0200

    Create config-allert.js

---
+++ config-allert.js
@@ -0,0 +1,6 @@
+window.ALERT_CONFIG = {
+  active: true, // true o false
+  type: "warning", // info | warning | error | success (stile USWDS)
+  heading: "Manutenzione in corso",
+  message: "Il sito sarà offline dalle 14:00 alle 15:00 per aggiornamenti.",
+};
